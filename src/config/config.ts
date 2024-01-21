e1ab546ad209d308891124ee1ade1b1ccaf430d3
Jarl André Hübenthal <jarlah@protonmail.com>
2024-01-21 21:09:14 +0100

    doc: update documentation

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,9 +2,9 @@ export const Config = {
 
     parseOptions: <THIZ extends Record<string, any>, T extends Record<string, any>>(thiz: THIZ, options: T) => {
         Object.keys(options).forEach(key => {
-            // We add underscore to the key if its not a readable property in thiz
+            // We add underscore to the key if its not a property directly defined in thiz,
             // to enable setting private variables, that begin with underscore.
-            // This was previously using the `in` keyword which returned true if field was inherited.
+            // This was previously using the `in` keyword which returned true if field was inherited from interface.
             // Now using hasOwnProperty to ensure its defined in the object/class passed in as thiz.
             // NOTE: This is a huge hack and goes against the value of using Typescript
             const option = thiz.hasOwnProperty(key) ? key : `_${key}`;
