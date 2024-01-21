66ca8002687c2e80d3cb73df9f68517a7ac64511
Jarl André Hübenthal <jarlah@protonmail.com>
2024-01-21 21:08:01 +0100

    fix: add proper test and bring back some previous code

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,8 +2,14 @@ export const Config = {
 
     parseOptions: <THIZ extends Record<string, any>, T extends Record<string, any>>(thiz: THIZ, options: T) => {
         Object.keys(options).forEach(key => {
-            if (thiz.hasOwnProperty(key)) {
-                thiz[key as keyof THIZ] = options[key];
+            // We add underscore to the key if its not a readable property in thiz
+            // to enable setting private variables, that begin with underscore.
+            // This was previously using the `in` keyword which returned true if field was inherited.
+            // Now using hasOwnProperty to ensure its defined in the object/class passed in as thiz.
+            // NOTE: This is a huge hack and goes against the value of using Typescript
+            const option = thiz.hasOwnProperty(key) ? key : `_${key}`;
+            if (option in thiz) {
+                thiz[option as keyof THIZ] = options[key];
             }
         });
     },
