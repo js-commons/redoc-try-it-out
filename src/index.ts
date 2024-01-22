e5e90a9080ac53025bbc751b077d3b0248d03cca
Jarl André Hübenthal <jarlah@protonmail.com>
2024-01-22 21:48:21 +0100

    build dist

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export const tryItOutJsFileName = 'try-it-out.js';
-export const tryItOutJsMinFileName = 'try-it-out.min.js';
 export * from './interfaces/auth-btn-options.interface';
 export * from './interfaces/redoc-try-it-out-options.interface';
 export * from './interfaces/style-matcher-options.interface';
